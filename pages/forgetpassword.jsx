d20c1b1bd4510b0b5bf22e4714aa117808cb362a
664Parvez <parvezrabbi664@gmail.com>
2023-06-25 12:10:27 +0600

    first time

---
+++ pages/forgetpassword.jsx
@@ -0,0 +1,12 @@
+// Components
+import HeadStaff from '../components/HeadStaff'
+
+const ForgetPassword = () => {
+    return (
+        <>
+            <HeadStaff title="Forget Password" />
+        </>
+    )
+}
+
+export default ForgetPassword
---
+++ pages/signup.jsx
@@ -0,0 +1,12 @@
+// Components
+import HeadStaff from '../components/HeadStaff'
+
+const Signup = () => {
+    return (
+        <>
+            <HeadStaff title="Signup" />
+        </>
+    )
+}
+
+export default Signup
